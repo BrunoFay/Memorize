f87d890695367da39e5cae494716a3bee6243560
Bruno Alves Fay <brunofay1@hotmail.com>
2024-05-11 16:58:47 -0300

    feat: add choose card behavior

--- Memorize/EmojiMemoryGameView.swift
+++ Memorize/EmojiMemoryGameView.swift
@@ -7,6 +7,7 @@ struct EmojiMemoryGameView: View {
         VStack {
             ScrollView {
                 cards
+                    .animation(.default, value: viewModel.cards)
                 Button("Shuffle") {
                     viewModel.shuffle()
                 }
@@ -20,10 +21,13 @@ struct EmojiMemoryGameView: View {
     
     var cards: some View {
         LazyVGrid(columns: [GridItem(.adaptive(minimum: 85), spacing: 0)], spacing: 0) {
-            ForEach(viewModel.cards.indices, id: \.self) { index in
-                CardView(viewModel.cards[index])
-                    .aspectRatio(2/3,contentMode: .fit)
+            ForEach(viewModel.cards) { card in
+                CardView(card)
+                    .aspectRatio(2/3, contentMode: .fit)
                     .padding(4)
+                    .onTapGesture {
+                        viewModel.choose(card)
+                    }
             }
         }
         .foregroundColor(.purple)
@@ -55,6 +59,7 @@ struct CardView: View {
             .opacity(card.isFaceUp ? 1 : 0)
             base.fill().opacity(card.isFaceUp ? 0 : 1)
         }
+        .opacity(card.isMatched ? 0 : 1 )
     }
 }
 
--- Memorize/MemoryGame.swift
+++ Memorize/MemoryGame.swift
@@ -7,30 +7,55 @@
 
 import Foundation
 
-struct MemoryGame<CarContent> {
+struct MemoryGame<CarContent> where CarContent: Equatable {
     private(set) var cards: [Card]
     
     init(numberOfPairOfCards: Int, cardContentFactory: (Int) -> CarContent) {
         cards = []
         
         for pairIndex in 0..<max(2, numberOfPairOfCards){
-            cards.append(Card(content: cardContentFactory(pairIndex)))
-            cards.append(Card(content: cardContentFactory(pairIndex)))
+            cards.append(Card(content: cardContentFactory(pairIndex), id: "\(pairIndex)A"))
+            cards.append(Card(content: cardContentFactory(pairIndex), id: "\(pairIndex)B"))
         }
     }
     
-    func choose(_ card: Card) {
-        
+    var indexOfTheOneAndOnlyFaceUpCard: Int? {
+        get { cards.indices.filter { index in cards[index].isFaceUp }.only }
+        set { cards.indices.forEach { cards[$0].isFaceUp = (newValue == $0) } }
+    }
+    
+    mutating func choose(_ card: Card) {
+        if let chosenIndex = cards.firstIndex(where: { $0.id == card.id }) {
+            if !cards[chosenIndex].isFaceUp && !cards[chosenIndex].isMatched {
+                if let potentialMatchIndex = indexOfTheOneAndOnlyFaceUpCard {
+                    if cards[chosenIndex].content == cards[potentialMatchIndex].content {
+                        cards[chosenIndex].isMatched = true
+                        cards[potentialMatchIndex].isMatched = true
+                    }
+                } else {
+                    indexOfTheOneAndOnlyFaceUpCard = chosenIndex
+                }
+                cards[chosenIndex].isFaceUp = true
+            }
+        }
     }
     
     mutating func shuffle() {
         cards.shuffle()
     }
     
-    struct Card {
-        var isFaceUp = true
+    struct Card: Equatable, Identifiable {
+        var isFaceUp = false
         var isMatched = false
         let content: CarContent
         
+        var id: String
+        
+    }
+}
+
+extension Array {
+    var only: Element? {
+        count == 1 ? first : nil
     }
 }
